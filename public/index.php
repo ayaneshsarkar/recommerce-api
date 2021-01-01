1dd36588e51574394a15c4359899784e4e8d1b61
Ayanesh Sarkar <ayaneshsarkar101@gmail.com>
2021-01-01 19:18:19 +0530

    Initial Commit :heavy_check_mark:

--- public/index.php
+++ public/index.php
@@ -7,7 +7,6 @@
     use App\Controllers\BooksController;
     use App\Controllers\CategoriesController;
     use App\Controllers\UsersController;
-    use App\Controllers\TestController;
     
     $env = Dotenv::createImmutable(dirname(__DIR__));
     $env->load();
@@ -15,7 +14,6 @@
     // dirname($_SERVER['DOCUMENT_ROOT'])
     $route = new Route();
 
-    $route->get('/abc/:id', [TestController::class, 'test']);
     $route->get('/get-books', [BooksController::class, 'getBooks']);
     $route->get('/get-categories', [CategoriesController::class, 'getCategories']);
     $route->get('/get-users', [UsersController::class, 'getUsers']);
